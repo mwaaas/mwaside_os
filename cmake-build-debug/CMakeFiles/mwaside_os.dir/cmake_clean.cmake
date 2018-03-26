da619cf83ac87896d10ce032f9b0858ea8794064
francismwangi <francismwangi152@gmail.com>
2018-03-26 10:49:59 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/mwaside_os.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mwaside_os.dir/main.c.o"
+  "mwaside_os.pdb"
+  "mwaside_os"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mwaside_os.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ devops/bootstrap_vagrant.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get install -y build-essential nasm genisoimage bochs bochs-sdl
